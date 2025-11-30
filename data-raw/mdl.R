b3e879811bca4ed990428ebe2ceddbba9969dac5
Raphael Hartmann <raphael.hartmann@protonmail.com>
2025-11-30 10:35:43 +0100

    chose another example model to have multiple trees

--- data-raw/mdl.R
+++ data-raw/mdl.R
@@ -1,8 +1,13 @@
 ## code to prepare `mdl` dataset goes here
 
 mdl_object <- "
+
 (1-K)*F + (1-K)*(1-F)*g # first line
 K # some comment
+(1-K)*(1-F)* (1-g)
+ # separation
+(1-K)*(1-F)*g # first line
+K + (1-K)*F
 (1-K)*(1-F)* (1-g)
 
 "
